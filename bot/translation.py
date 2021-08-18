096548f1241b0159d8c2951c864d202e39a425a3
Hitenop <87300913+Hitenop@users.noreply.github.com>
2021-08-18 14:58:09 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -48,11 +48,11 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AnnihilusOP">AnnihilusOP</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/hiten_op">Hiten</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Channel</b> : <i><a href="https://t.me/MoviZenX">Click Here</a></i>
+<b>➥ Channel</b> : <i><a href="https://t.me/request_moviesorseries">Click Here</a></i>
 """
